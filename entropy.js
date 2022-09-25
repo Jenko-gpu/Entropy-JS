d9c1a7aca70dc0d693dbc7833ee0f0b0e669fdc5
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2022-09-25 21:12:53 +0500

    Update entropy.js

--- entropy.js
+++ entropy.js
@@ -1,24 +1,22 @@
-
-
 var filename = process.argv[2];
 
 var fs = require('fs');
 
-fs.readFile(filename, (err,data)=> {
+fs.readFile(filename, (err,data) => {
     if (err) {
         console.log(`File ${filename} doesn\'t exist`);
     } else {
         Text = data.toString();
         Letters = new Array();
         let i = 0;
-
+        let n = 0;
         while (i<Text.length){
             if (Letters[Text.charAt(i)]) {
 
                 Letters[Text.charAt(i)]+=1;
 
             } else{
-
+                n++;
                 Letters[Text.charAt(i)] = 1;
             }
             i++;
@@ -28,9 +26,10 @@ fs.readFile(filename, (err,data)=> {
         if (i>=2) {
             for ( j in Letters) {
                 Letters[j]/=i;
-                Entropy -= Letters[j] * (Math.log(Letters[j])/Math.log(i));
+                Entropy -= Letters[j] * (Math.log(Letters[j]) / Math.log(n));
             }
         }
         console.log(`Entropy is ${Entropy}`);
     }
+    
 })
